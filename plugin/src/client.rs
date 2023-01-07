6d9f7a36099f6e493e38505c0ce8d345a3f0d951
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-01-07 17:46:32 +0100

    fix(plugin): code cleanup
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- plugin/src/client.rs
+++ plugin/src/client.rs
@@ -1,4 +1,4 @@
-//! Client dispach implementation
+//! Client dispatch implementation
 use clightningrpc_plugin::errors::PluginError;
 use clightningrpc_plugin::plugin::Plugin;
 use future_common::client::FutureBackend;
--- plugin/src/plugin.rs
+++ plugin/src/plugin.rs
@@ -1,20 +1,27 @@
 //! Plugin definition.
-use clightningrpc_plugin::{
-    commands::RPCCommand, errors::PluginError, plugin::Plugin, types::LogLevel,
-};
+use clightningrpc_plugin::commands::RPCCommand;
+use clightningrpc_plugin::errors::PluginError;
+use clightningrpc_plugin::plugin::Plugin;
+use clightningrpc_plugin::types::LogLevel;
 use future_common::client::FutureBackend;
 
-#[derive(Clone)]
 pub struct PluginState<'tcx> {
-    client: &'tcx Option<Box<dyn FutureBackend<PluginState<'tcx>, Error = PluginError>>>,
+    client: Option<Box<dyn FutureBackend<PluginState<'tcx>, Error = PluginError>>>,
 }
 
 impl PluginState<'_> {
     fn new() -> Self {
-        PluginState { client: &None }
+        PluginState { client: None }
     }
 }
 
 pub fn build_plugin<'c>() -> Plugin<PluginState<'c>> {
-    todo!()
+    let plugin = Plugin::new(PluginState::new(), false);
+    plugin
+}
+
+impl<'tcx> Clone for PluginState<'tcx> {
+    fn clone(&self) -> Self {
+        self.to_owned()
+    }
 }
