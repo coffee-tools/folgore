00e20ea2b942b89ccc8298314539b774b1493ca8
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-06 18:58:13 +0200

    fixup! make a first call to estimate the fee
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- satoshi-nakamoto/src/lib.rs
+++ satoshi-nakamoto/src/lib.rs
@@ -1,27 +1,35 @@
 //! Future client implementation for nakamoto
+
+use std::cell::Cell;
+use std::collections::BTreeMap;
+use std::net::TcpStream;
+
+use clightningrpc_plugin::types::LogLevel;
+use nakamoto_client::handle::Handle;
+use nakamoto_client::model::Tip;
+pub use nakamoto_client::Config;
+pub use nakamoto_client::{Client, Error, Event, Network};
+use nakamoto_common::bitcoin::consensus::{deserialize, serialize};
+use nakamoto_common::bitcoin_hashes::hex::ToHex;
+use nakamoto_common::block::{BlockHash, Height, Transaction};
+use nakamoto_net_poll::{Reactor, Waker};
+use nakamoto_p2p::fsm::fees::FeeEstimate;
+use serde_json::{json, Value};
+
 use clightningrpc_common::json_utils;
 use clightningrpc_plugin::error;
 use clightningrpc_plugin::errors::PluginError;
 use clightningrpc_plugin::plugin::Plugin;
-use nakamoto_client::handle::Handle;
-use nakamoto_client::model::Tip;
-pub use nakamoto_client::{Client, Error, Event, Network};
-use nakamoto_common::bitcoin::consensus::{deserialize, serialize};
-use nakamoto_common::block::Transaction;
-use nakamoto_net_poll::{Reactor, Waker};
-use nakamoto_p2p::fsm::fees::FeeEstimate;
+
 use satoshi_common::client::SatoshiBackend;
-use serde_json::Value;
-use std::net::TcpStream;
-
-pub use nakamoto_client::Config;
-
 use satoshi_common::utils::{bitcoin_hashes, hex};
 
 #[derive(Clone)]
 pub struct Nakamoto {
     network: Network,
     handler: nakamoto_client::Handle<Waker>,
+    fees: BTreeMap<Height, FeeEstimate>,
+    last_hash: Cell<Option<BlockHash>>,
 }
 
 impl Nakamoto {
@@ -31,7 +39,12 @@ impl Nakamoto {
         let network = config.network;
         // FIXME: join this later
         let _worker = std::thread::spawn(|| nakamoto.run(config));
-        let client = Nakamoto { handler, network };
+        let client = Nakamoto {
+            handler,
+            network,
+            fees: BTreeMap::new(),
+            last_hash: Cell::new(None),
+        };
 
         Ok(client)
     }
@@ -51,6 +64,19 @@ impl Nakamoto {
         Ok(resp)
     }
 
+    fn null_estimate_fees(&self) -> Result<Value, PluginError> {
+        Ok(json!({
+            "opening": null,
+            "mutual_close": null,
+            "unilateral_close": null,
+            "delayed_to_us": null,
+            "htlc_resolution": null,
+            "penalty": null,
+            "min_acceptable": null,
+            "max_acceptable": null,
+        }))
+    }
+
     // FIXME: return the correct error
     pub fn stop(self) -> Result<(), Error> {
         self.handler.shutdown()?;
@@ -59,7 +85,11 @@ impl Nakamoto {
 }
 
 impl<T: Clone> SatoshiBackend<T> for Nakamoto {
-    fn sync_block_by_height(&self, _: &mut Plugin<T>, height: u64) -> Result<Value, PluginError> {
+    fn sync_block_by_height(
+        &self,
+        plugin: &mut Plugin<T>,
+        height: u64,
+    ) -> Result<Value, PluginError> {
         let mut response = json_utils::init_payload();
         let header = self.handler.get_block_by_height(height).unwrap();
         let blk_chan = self.handler.blocks();
@@ -74,6 +104,7 @@ impl<T: Clone> SatoshiBackend<T> for Nakamoto {
             return Err(err);
         }
 
+        self.last_hash.set(Some(header.block_hash()));
         json_utils::add_str(
             &mut response,
             "blockhash",
@@ -82,8 +113,8 @@ impl<T: Clone> SatoshiBackend<T> for Nakamoto {
 
         let (blk, _) = blk_chan.recv().unwrap();
         let serialize = serialize(&blk);
-        let ser_str = std::str::from_utf8(&serialize).unwrap();
-        json_utils::add_str(&mut response, "block", ser_str);
+        let ser_str = serialize.as_slice().to_hex();
+        json_utils::add_str(&mut response, "block", &ser_str);
         Ok(response)
     }
 
@@ -111,11 +142,20 @@ impl<T: Clone> SatoshiBackend<T> for Nakamoto {
     }
 
     fn sync_estimate_fees(&self, _: &mut Plugin<T>) -> Result<Value, PluginError> {
-        loop {
-            if let Ok(Event::FeeEstimated { fees, .. }) = self.handler.events().recv() {
-                return self.build_estimate_fees(fees);
+        let Some(last_hash) = self.last_hash.get() else {
+            return self.null_estimate_fees();
+        };
+        let _ = self.handler.request_block(&last_hash);
+        let Ok((_, Some(fees))) = self.handler.wait(|event| {
+            if let nakamoto_p2p::Event::Inventory(nakamoto_p2p::fsm::InventoryEvent::BlockProcessed { height, fees, ..}) = event {
+                Some((height, fees))
+            } else {
+                None
             }
-        }
+        }) else {
+            return self.null_estimate_fees();
+        };
+        self.build_estimate_fees(fees)
     }
 
     fn sync_get_utxo(&self, _: &mut Plugin<T>, _: &str, _: u64) -> Result<Value, PluginError> {
@@ -133,7 +173,7 @@ impl<T: Clone> SatoshiBackend<T> for Nakamoto {
         let mut resp = json_utils::init_payload();
         if let Err(err) = self.handler.submit_transaction(tx) {
             json_utils::add_bool(&mut resp, "success", false);
-            json_utils::add_str(&mut resp, "errmsg", format!("{err}").as_str());
+            json_utils::add_str(&mut resp, "errmsg", &format!("{err}"));
         } else {
             json_utils::add_bool(&mut resp, "success", true);
         }
