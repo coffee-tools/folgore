a3f306f7f8ee6758941562b10df6929d1f0c2af3
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-06 02:57:26 +0200

    fix(nakamoto): support the nakamoto client
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- satoshi-common/src/lib.rs
+++ satoshi-common/src/lib.rs
@@ -1,1 +1,9 @@
 pub mod client;
+
+pub mod utils {
+    pub use bitcoin_hashes;
+
+    #[macro_export]
+    macro_rules! hex (($hex:expr) => (<Vec<u8> as bitcoin_hashes::hex::FromHex>::from_hex($hex).unwrap()));
+    pub use hex;
+}
