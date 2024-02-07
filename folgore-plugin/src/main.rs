bcb70631bcef2511d2cd0d7292e99a5521fef8e3
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-02-07 18:19:31 +0100

    fix(plugin): fix retry logic
    Link: https://github.com/coffee-tools/folgore/issues/51
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- folgore-plugin/src/main.rs
+++ folgore-plugin/src/main.rs
@@ -7,3 +7,16 @@ fn main() {
     let plugin = plugin::build_plugin();
     plugin.start();
 }
+
+#[cfg(test)]
+use std::sync::Once;
+
+#[cfg(test)]
+static INIT: Once = Once::new();
+
+#[cfg(test)]
+fn configure_tests() {
+    INIT.call_once(|| {
+        env_logger::init();
+    });
+}
